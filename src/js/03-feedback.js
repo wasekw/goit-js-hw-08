54fac65375422b4d623f59290a44858c5f3da4f0
Vasiliy Volobaev <vasiliy.volobaev@gmail.com>
2021-10-31 15:25:57 +0200

    Done 3 task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,18 +2,32 @@
 var  throttle = require('lodash.throttle');
 
 const feedbackForm = document.querySelector('.feedback-form');
-console.log(feedbackForm);
 const LOCALSTORAGE_KEY = "feedback-form-state";
+const saveObject = {};
 
+initForm ()
 
-feedbackForm.addEventListener('input', catchDataForm);
+feedbackForm.addEventListener('input', throttle(catchDataForm, 500));
+feedbackForm.addEventListener('submit', () => {
+ localStorage.removeItem(LOCALSTORAGE_KEY);
+})
 
 function catchDataForm (event) {
   event.preventDefault();
-  const formData = new formData(feedbackForm);
-  console.log(formData);
-
-
+saveObject[event.target.name] = event.target.value;
+localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(saveObject));
 }
+
+function initForm () {
+      let fillFieldsForm = localStorage.getItem(LOCALSTORAGE_KEY);
+      if (fillFieldsForm) {
+        fillFieldsForm = JSON.parse(fillFieldsForm);
+      Object.entries(fillFieldsForm).forEach(([name, value])=> {
+        saveObject[name] = value;
+        feedbackForm.elements[name].value = value;
+      })
+    }
+  }
+
 
 
